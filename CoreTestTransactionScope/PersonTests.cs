909c34c5eb747ab7c774d4744951b1a75bd6803a
Oliver Baylis <oli.baylis@gmail.com>
2019-06-06 21:08:18 +0100

    With transaction scope

--- CoreTestTransactionScope/PersonTests.cs
+++ CoreTestTransactionScope/PersonTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using System.Transactions;
 using Xunit;
 
 namespace CoreTestTransactionScope
@@ -9,14 +10,17 @@ namespace CoreTestTransactionScope
         [Fact]
         public async Task CreatePerson()
         {
-            var updater = new DatabaseUpdater();
-            var result = await updater.CreatePersonAsync(new Person
+            using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
             {
-                FullName = Guid.NewGuid().ToString(),
-                DateUpdated = DateTime.Now
-            });
+                var updater = new DatabaseUpdater();
+                var result = await updater.CreatePersonAsync(new Person
+                {
+                    FullName = Guid.NewGuid().ToString(),
+                    DateUpdated = DateTime.Now
+                });
 
-            Assert.True(result.Id > 0);
+                Assert.True(result.Id > 0);
+            }
         }
     }
 }
